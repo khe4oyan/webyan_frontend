8cf739db13564234678df305af17613341ac47b4
Suren <khe4oyan@gmail.com>
2025-12-09 23:05:08 +0400

    [feat] submit button fix

--- js/form.js
+++ js/form.js
@@ -1,19 +1,21 @@
-{
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector('.feedBack .form');
-
+  
   form.addEventListener('submit', async (e) => {
     e.preventDefault(); // отменяем стандартную отправку (без перезагрузки)
 
     const formData = Object.fromEntries(new FormData(form).entries());
+    const responseEl = document.querySelector('.response');
+    const submitBtnDOM = document.querySelector(".sendButton");
 
     try {
+      submitBtnDOM.disabled = true;
       const res = await fetch('https://carparts-t0mi.onrender.com/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
       });
 
-      const responseEl = document.querySelector('.response');
       console.log(res);
 
       if (res.ok) {
@@ -23,7 +25,10 @@
         responseEl.textContent = '❌ Server is not working';
       }
     } catch (err) {
+      console.log(err.message);
       responseEl.textContent = '⚠️ Server not response';
+    } finally {
+      submitBtnDOM.disabled = false;
     }
   });
-}
+});
